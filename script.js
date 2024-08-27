5d7945b16960fcc9ee0fc72aa3f39d04e0e1dd37
TruffleBug <lisa.ho1022@gmail.com>
2024-08-27 17:14:19 -0600

    Add UI for game
    Changed method of play from in the console to UI on webpage using JS.
    This version is not complete -- scores do not update on webpage, but do
    in console. Also game does not end at 5 rounds.

--- script.js
+++ script.js
@@ -1,79 +1,61 @@
-// Declare a function called getComputerChoice
-function getComputerChoice () {
-    // Declare a variable, randomNum, that has a value of a random number between 0 and 0.99
-    let randomNum = Math.random();
-    // Declare a variable, compChoice, with a value of an empty string
-    let compChoice = '';
-    // If randomNum is between 0 and 0.33
-    if (randomNum >=0 && randomNum<0.34) {
-        // Set compChoice equal to 'Rock'
-        compChoice = 'rock';
-        // Else if randomNum is between 0.34 and 0.66
-    } else if (randomNum >= 0.34 && randomNum <0.67) {
-        // Set compChoice equal to 'Paper'
-        compChoice = 'paper';        
-        // Else 
-    } else {
-        // Set compChoice equal to 'Scissors'
-        compChoice = 'scissors';
-    }
-    // Return compChoice
-    return compChoice 
-}
-
-// Declare a function called getHumanChoice
-function getHumanChoice() {
-    // Declare a variable, humanChoice, to be result of use prompt enter one of three choices: 'Rock', 'Paper', or 'Scissors'
-    let humanChoice = prompt('Choose wisely.', 'Enter Rock, Paper, or Scissors')
-    if (humanChoice.toLowerCase() === 'rock' ||
-        humanChoice.toLowerCase() === 'paper' ||
-        humanChoice.toLowerCase() === 'scissors') {
-            // return humanChoice in all lowercase
-            return humanChoice.toLowerCase();
-    } else {
-        alert('Try again')
-        getHumanChoice()
-    }
-}
-
-// Declare a function, playRound
-function playRound() {
-    compChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
-    // if humanChoice = rock & compChoice = scissors  
-    if (humanChoice === 'rock' && compChoice === 'scissors' ||
-        // or humanChoice = paper & compChoice = rock
-        humanChoice === 'paper' && compChoice === 'rock' ||
-        // or humanChoice = scissors & compChoice = paper
-        humanChoice === 'scissors' && compChoice === 'paper') {
-            // increment humanScore by 1
-            ++humanScore
-            // return 'You win! [humanChoice] beats [compChoice]'
-            return console.log(`You win :D ${humanChoice} beats ${compChoice}`)
-    // else if humanChoice = compChoice
-    } else if (humanChoice === compChoice) {
-        // return 'It's a tie!'
-        return console.log('It\'s a tie!')
-        // else
-    } else {
-        // increment computerScore by 1
-        ++computerScore
-        // return 'You lose! [compChoice] beats [humanChoice]'
-        return console.log(`You lose :( ${compChoice} beats ${humanChoice}`)
-    }
-}
-
+let humanChoice = '';
 let humanScore = 0;
 let computerScore = 0;
 
-// Declare a function, playGame
+function getComputerChoice () {
+    let randomNum = Math.random();
+    let compChoice = '';
+    if (randomNum >=0 && randomNum<0.34) {
+        compChoice = 'rock';
+    } else if (randomNum >= 0.34 && randomNum <0.67) {
+        compChoice = 'paper';        
+    } else {
+        compChoice = 'scissors';
+    }
+    return compChoice 
+}
+
+// function getHumanChoice() {
+    //     let humanChoice = prompt('Choose wisely.', 'Enter Rock, Paper, or Scissors')
+    //     if (humanChoice.toLowerCase() === 'rock' ||
+    //         humanChoice.toLowerCase() === 'paper' ||
+    //         humanChoice.toLowerCase() === 'scissors') {
+        //             return humanChoice.toLowerCase();
+        //     } else {
+            //         alert('Try again')
+            //         getHumanChoice()
+            //     }
+            // }
+            
+function playRound() {
+    compChoice = getComputerChoice();
+    // humanChoice = getHumanChoice();
+    if (humanChoice === 'rock' && compChoice === 'scissors' ||
+        humanChoice === 'paper' && compChoice === 'rock' ||
+        humanChoice === 'scissors' && compChoice === 'paper') {
+            ++humanScore;
+            console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
+            resultMsg.textContent = `You win :D ${humanChoice} beats ${compChoice}`;
+            // return console.log(`You win :D ${humanChoice} beats ${compChoice}`)
+    } else if (humanChoice === compChoice) {
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
+        resultMsg.textContent = `It\'s a tie!`
+        // return console.log('It\'s a tie!')
+    } else {
+        ++computerScore;
+        console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
+        resultMsg.textContent = `You lose :( ${compChoice} beats ${humanChoice}`
+        // return console.log(`You lose :( ${compChoice} beats ${humanChoice}`)
+    }
+}
+
 function playGame() {
     for (let i=1; i<6; i++) {
-        playRound()    
-        console.log(`
-        Human Score: ${humanScore}
-        Computer Score: ${computerScore}
-        ---------------`)
+        // playRound()    
+        // console.log(`
+        // Human Score: ${humanScore}
+        // Computer Score: ${computerScore}
+        // ---------------`)
     }
     if (humanScore > computerScore) {
         return alert('YOU\'RE A WINNER!')
@@ -84,4 +66,32 @@ function playGame() {
     }
 }
 
-playGame();
+const buttonRock = document.querySelector('#buttonRock');
+buttonRock.addEventListener('click', () => {
+    humanChoice = 'rock';
+    playRound();
+})
+
+const buttonPaper = document.querySelector('#buttonPaper');
+buttonPaper.addEventListener('click', () => {
+    humanChoice = 'paper';
+    playRound();
+})
+
+const buttonScissors = document.querySelector('#buttonScissors');
+buttonScissors.addEventListener('click', () => {
+    humanChoice = 'scissors';
+    playRound();
+})
+
+const resultsContainer = document.querySelector('div');
+
+const resultMsg = document.createElement('p');
+resultsContainer.appendChild(resultMsg);
+
+const results = document.createElement('p');
+results.textContent = `Human Score: ${humanScore}, Computer Score: ${computerScore}`;
+resultsContainer.appendChild(results);
+
+// document.addEventListener('load', playGame);
+playGame()
